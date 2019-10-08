2c64a8229ff7bf651543d55dd7a53345fd7c6ce8
N Fikri <fikri.winaji@gmail.com>
2019-10-08 21:32:43 +0700

    www

--- app/src/main/java/com/proyek/ibf/Chat.java
+++ app/src/main/java/com/proyek/ibf/Chat.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import android.support.v7.app.AppCompatActivity;
 
+import com.appsnipp.education.R;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
